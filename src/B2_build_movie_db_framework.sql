55dddda5c6418d00b935d05f7af668e7e8018a05
Julius Krüger Madsen <1230150922@protonmail.com>
2024-09-27 10:04:03 +0200

    refactor: refactored the create script based on the PR feedback recieved. Removed the formatted domain model png

--- src/B2_build_movie_db_framework.sql
+++ src/B2_build_movie_db_framework.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS search_history CASCADE;
+DROP TABLE IF EXISTS bookmark CASCADE;
+DROP TABLE IF EXISTS completed CASCADE;
+DROP TABLE IF EXISTS plan_to_watch CASCADE;
+DROP TABLE IF EXISTS user_score CASCADE;
+
 CREATE TABLE "user" (
     user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -6,13 +13,13 @@ CREATE TABLE "user" (
 );
 
 CREATE TABLE search_history (
-    search_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    search_history_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER REFERENCES "user"(user_id) ON DELETE CASCADE,
-    status VARCHAR(50),
-    note TEXT
+    type VARCHAR(50),
+    query TEXT
 );
 
-CREATE TABLE bookmarks (
+CREATE TABLE bookmark (
     bookmark_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER REFERENCES "user"(user_id) ON DELETE CASCADE,
     media_id INTEGER REFERENCES media(id) ON DELETE CASCADE,
@@ -23,16 +30,16 @@ CREATE TABLE completed (
     completed_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     bookmark_id INTEGER REFERENCES bookmarks(bookmark_id) ON DELETE CASCADE,
     completed_date DATE NOT NULL,
-    rewatchability INTEGER CHECK (rewatchability >= 1 AND rewatchability <= 10)
+    rewatchability INTEGER CHECK (rewatchability >= 1 AND rewatchability <= 5)
 );
 
 CREATE TABLE plan_to_watch (
-    plan_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    plan_to_watch_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     bookmark_id INTEGER REFERENCES bookmarks(bookmark_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_score (
-    score_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_score_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER REFERENCES "user"(user_id) ON DELETE CASCADE,
     media_id INTEGER REFERENCES media(id) ON DELETE CASCADE,
     score_value INTEGER CHECK (score_value >= 1 AND score_value <= 10),
