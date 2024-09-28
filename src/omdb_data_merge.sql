585faf919f1361100a5ef79c16df2abf4829aec4
Marek <marek@admins-MacBook-Pro-2.local>
2024-09-29 01:56:22 +0200

    fixing spaces in the code

--- src/omdb_data_merge.sql
+++ src/omdb_data_merge.sql
@@ -3,22 +3,22 @@
 DO $$
 UPDATE media
 SET plot = (CASE
-				WHEN o.plot = 'N/A' THEN NULL
-				WHEN o.plot = '' THEN NULL
-                ELSE o.plot END),
+	    WHEN o.plot = 'N/A' THEN NULL
+	    WHEN o.plot = '' THEN NULL
+        ELSE o.plot END),
     box_office = CAST(CASE 
-                WHEN REPLACE(TRIM('$' FROM o.boxoffice), ',', '') = 'N/A' THEN NULL 
-                ELSE REPLACE(TRIM('$' FROM o.boxoffice), ',', '') 
-                END AS INTEGER),
+        WHEN REPLACE(TRIM('$' FROM o.boxoffice), ',', '') = 'N/A' THEN NULL 
+        ELSE REPLACE(TRIM('$' FROM o.boxoffice), ',', '') 
+        END AS INTEGER),
     website = (CASE
-				WHEN o.website= 'N/A' THEN NULL 
-                ELSE o.website END),
+		WHEN o.website= 'N/A' THEN NULL 
+        ELSE o.website END),
     awards = (CASE
-				WHEN o.awards= 'N/A' THEN NULL 
-                ELSE o.awards END),
+		WHEN o.awards= 'N/A' THEN NULL 
+        ELSE o.awards END),
     poster = (CASE
-				WHEN o.poster= 'N/A' THEN NULL 
-                ELSE o.poster END)
+		WHEN o.poster= 'N/A' THEN NULL 
+        ELSE o.poster END)
 FROM original.omdb_data AS o
 WHERE media.imdb_id = o.tconst;
 $$;
