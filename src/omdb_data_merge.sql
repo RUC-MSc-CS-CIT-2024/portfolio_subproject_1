d0dce4ea2831bd16f22ba22ae4b2cb4740d9ac5d
Marek <marek@admins-MacBook-Pro-2.local>
2024-09-30 23:41:06 +0200

    posters, websites, production_company population

--- src/omdb_data_merge.sql
+++ src/omdb_data_merge.sql
@@ -33,7 +33,7 @@ ON media.imdb_id = o.tconst
 WHERE release.media_id = media.media_id;
 
 --Update release_date with the correct dates from omdb
---Insert additional releases 
+--Insert additional releases and create new releases if the dates are different
 WITH updated AS (
     UPDATE release
     SET release_date = o.released::date
@@ -45,12 +45,51 @@ WITH updated AS (
     AND o.released != ''
     RETURNING release.media_id
 )
-INSERT INTO release (title, release_date, country_code, media_id, rated)
-SELECT o.title, o.released::date, null, media.media_id, o.rated
-FROM original.omdb_data o
-JOIN media ON media.imdb_id = o.tconst
-LEFT JOIN updated u ON u.media_id = media.media_id
-WHERE o.released IS NOT NULL
-AND o.released != 'N/A' 
-AND o.released != ''
-AND media.media_id != u.media_id;
+INSERT INTO release (title, release_date, country_id, media_id, rated)
+    SELECT o.title, o.released::date, null, media.media_id, o.rated
+    FROM original.omdb_data o
+    JOIN media ON media.imdb_id = o.tconst
+    LEFT JOIN updated u ON u.media_id = media.media_id
+    WHERE o.released IS NOT NULL
+        AND o.released != 'N/A' 
+        AND o.released != ''
+        AND media.media_id != u.media_id;
+
+--PROMOTIONAL MEDIA
+
+/*
+***IDEA***
+What if we create a table and have types in there?
+
+related_media_category(categoryname, parentcategory)
+
+We can have then images, websites, videos without parentcat
+and subcategories like poster, actor, director, premiere for images
+and etc... 
+*/
+
+--insert posters
+INSERT INTO promotional_media(release_id, "type", uri)
+SELECT r.release_id, 'poster', o.poster
+FROM release r 
+JOIN media m ON r.media_id = m.media_id
+JOIN original.omdb_data o ON m.imdb_id = o.tconst
+WHERE o.poster != 'N/A'
+    AND o.poster != ''
+    AND o.poster IS NOT NULL;
+
+--insert websites
+INSERT INTO promotional_media(release_id, "type", uri)
+SELECT r.release_id, 'website', o.website
+FROM release r 
+JOIN media m ON r.media_id = m.media_id
+JOIN original.omdb_data o ON m.imdb_id = o.tconst
+WHERE o.website != 'N/A'
+    AND o.website != ''
+    AND o.website IS NOT NULL;
+
+--PRODUCTION COMPANY
+
+INSERT INTO production_compnay ("name", "description")
+SELECT DISTINCT unnest(string_to_array(o.production, ', ')),
+FROM original.omdb_data as o;
