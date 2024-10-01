7efa7c759733d029908350f6af862fcbdaf8fc5a
MarLaslo <168294377+MarLaslo@users.noreply.github.com>
2024-10-01 17:38:04 +0200

    feat:creating simple search function D2 (#19)
    creating simple search function D2
    
    Co-authored-by: Marek <marek@admins-MacBook-Pro-2.local>

---
+++ src/D_functions_and_procedures.sql
@@ -0,0 +1,24 @@
+--D2 SIMPLE SEARCH
+
+CREATE OR REPLACE FUNCTION simple_search
+  (query varchar(100), user_id integer)
+RETURNS TABLE (media_id INTEGER, title TEXT)
+AS $$
+BEGIN
+  -- SEARCH HISTORY
+  INSERT INTO search_history (user_id, type, query)
+  VALUES (user_id, 'simple_search', query);
+
+  -- RESULT
+  RETURN QUERY
+  SELECT me.media_id, re.title
+  FROM media me 
+  JOIN "release" re USING (media_id)
+  WHERE re.title LIKE '%' || query || '%' 
+     OR me.plot LIKE '%' || query || '%';
+END;
+$$
+LANGUAGE 'plpgsql';
+
+--D2 TEST
+SELECT * FROM simple_search('apple',1);
