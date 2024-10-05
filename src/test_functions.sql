e98b36a9ac687fbf32f715d4a5cb91d12deb8b47
Julius Krüger Madsen <1230150922@protonmail.com>
2024-10-05 14:32:03 +0200

    feat: added test_functions.sql to test all functions and procedures.

---
+++ src/test_functions.sql
@@ -0,0 +1,8 @@
+-- test_functions.sql
+-- This script tests all functions and procedures created in functions.sql
+-- It demonstrates that each function works as intended.
+
+/timing
+
+-- Begin transaction
+BEGIN;
