4ac9e616287026ac1f1adf6215c99be2cb018d5a
Julius Krüger Madsen <1230150922@protonmail.com>
2024-10-05 23:08:31 +0200

    feat: simplified the script and removed the redundant splitting part

--- data/data.py
+++ data/data.py
@@ -1,21 +1,5 @@
 import os
 
-def split_file(file_path, chunk_size=50 * 1024 * 1024):
-    chunk_num = 1
-    chunk_files = []
-    with open(file_path, 'rb') as file:
-        while True:
-            chunk = file.read(chunk_size)
-            if not chunk:
-                break
-            chunk_file_name = f"{file_path}.part{chunk_num}"
-            with open(chunk_file_name, 'wb') as chunk_file:
-                chunk_file.write(chunk)
-            chunk_files.append(chunk_file_name)
-            chunk_num += 1
-    print(f"{file_path} has been split into {len(chunk_files)} parts.")
-    return chunk_files
-
 def reassemble_files(part_files, output_file):
     with open(output_file, 'wb') as outfile:
         for part_file in part_files:
@@ -23,14 +7,10 @@ def reassemble_files(part_files, output_file):
                 outfile.write(infile.read())
     print(f'Reassembled file saved as {output_file}')
 
-files_to_split = ['imdb.backup', 'wi.backup', 'omdb_data.backup']
-for file_path in files_to_split:
-    split_file(file_path)
-
 imdb_parts = [f'imdb.backup.part{n}' for n in range(1, 4)]
 wi_parts = [f'wi.backup.part{n}' for n in range(1, 4)]
 omdb_parts = [f'omdb_data.backup.part{n}' for n in range(1, 3)]
 
-reassemble_files(imdb_parts, 'imdb_reassembled.backup')
-reassemble_files(wi_parts, 'wi_reassembled.backup')
-reassemble_files(omdb_parts, 'omdb_data_reassembled.backup')
+reassemble_files(imdb_parts, 'imdb.backup')
+reassemble_files(wi_parts, 'wi.backup')
+reassemble_files(omdb_parts, 'omdb_data.backup')
