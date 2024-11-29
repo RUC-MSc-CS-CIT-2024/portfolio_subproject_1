8fef9fab14c7b5605d0c307df7ba5218523d4625
Sofus Hilfling <sofus@techchapter.com>
2024-11-29 10:00:18 +0100

    feat: add Dockerfile and setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Import data
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /data/imdb.backup
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /data/omdb_data.backup
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /data/wi.backup
+
+# Run scripts in order
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /scripts/B2_build_movie_db.sql
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /scripts/C2_build_framework.sql
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /scripts/D_functions_and_procedures.sql
