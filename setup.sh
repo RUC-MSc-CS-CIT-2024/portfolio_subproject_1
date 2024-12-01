b57f66f9dc63f7388cc50c907f34c049a6d210af
sofushn <sofushn@hotmail.dk>
2024-12-01 01:33:36 +0100

    feat: create flag file to signal startup finished

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /data/wi.backup
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /scripts/B2_build_movie_db.sql
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /scripts/C2_build_framework.sql
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /scripts/D_functions_and_procedures.sql
+
+touch /tmp/startup_finished
