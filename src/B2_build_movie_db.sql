6a5cba8ba4cfdffc74f4665500a749d9bdee9774
MarLaslo <168294377+MarLaslo@users.noreply.github.com>
2024-12-06 12:09:54 +0100

    hotfix: simple_search (#82)
    * simple_search adding if user id -1
    
    * removing search by plot containing the word
    
    * making the simple search split words and search to metch all
    
    * adding left join to media_primary_information population script
    
    * fixing date in BC bug
    
    * returning simple search back to its state

--- src/B2_build_movie_db.sql
+++ src/B2_build_movie_db.sql
@@ -137,7 +137,7 @@ CREATE TABLE title (
 
 CREATE TABLE release (
     release_id      INTEGER     PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    release_date    DATE        NOT NULL,
+    release_date    DATE        NULL,
     rated           VARCHAR(80) NULL,
     "type"          VARCHAR(50) NOT NULL,
     country_id      INTEGER     NULL REFERENCES country(country_id),
@@ -663,8 +663,8 @@ SELECT
     TO_DATE(startyear, 'YYYY'),
     'original',
     media_id
-FROM titleaka_with_id;
-
+FROM titleaka_with_id
+WHERE startyear != '';
 -- Seasons
 
 DO $$
@@ -678,7 +678,10 @@ BEGIN
         SELECT 
             e.parenttconst,
             e.seasonnumber AS season_number,
-            TO_DATE(MIN(t.startyear)::TEXT, 'YYYY') AS "start_date",  
+            CASE 
+                WHEN MIN(startyear) IS '' THEN NULL 
+                ELSE TO_DATE(MIN(startyear)::TEXT, 'YYYY') 
+            END,  
             s.primarytitle AS show_title,
             m.media_id
         FROM original.title_basics AS t
@@ -869,8 +872,8 @@ WITH
 INSERT INTO media_primary_information(media_id, title_id, release_id, promotional_media_id)
 SELECT pt.media_id, pt.title_id, pr.release_id, pp.promotional_media_id
 FROM primary_title AS pt
-JOIN primary_release AS pr USING(media_id)
-JOIN primary_poster AS pp USING(media_id);
+LEFT JOIN primary_release AS pr USING(media_id)
+LEFT JOIN primary_poster AS pp USING(media_id);
 
 
 -- Insert Production companies
